baaa328afc1a05530214516c5b42457925a2bde9
jiangtao <jiangtao@tao-studio.net>
2017-12-17 23:52:09 +0800

    remove all

--- assets/killers/index.js
+++
@@ -1,7 +0,0 @@
-let uikiller = require('./uikiller/uikiller');
-let net = require('./netkiller/netkiller');
-let pbkiller = require('./pbkiller/src/pbkiller');
-
-global.UIKiller = uikiller;
-global.pbkiller = pbkiller;
-global.net = net;
